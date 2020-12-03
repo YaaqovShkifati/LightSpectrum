7b4d89216b7a775be1916d2867bb8a95fa31605e
YaaqovShkifati <70407221+YaaqovShkifati@users.noreply.github.com>
2020-12-02 19:45:33 -0500

    Update LightSpectrum.java

--- LightSpectrum.java
+++ LightSpectrum.java
@@ -1,28 +1,4 @@
-/*
- * @author Yaaqov Shkifati
- * @since 11/10/19
- * @version 1.0
- * Decription: The first person I am aware of to grasp the scientific basis of 
-subtractive printing was the Scottish physicist James Clerk Maxwell. In the 
-mid-nineteenth century he stated that three widely spaced points in the spectrum 
-could be chosen, and that the colors at these points could be combined in 
-varying levels of brightness to produce virtually every color we can see. 
-In arguing this Maxwell also made the point that there are actually two 
-different sets of three primary colors. One set, used in painting, eventually 
-in printing, and later still in photography, is called the subtractive 
-primaries: cyan, magenta, and yellow. Please note that although cyan and 
-magenta are close relatives of blue and red, they are not the classic primaries 
-of the painters. The second set, used when mixing projected 
-(rather than reflected) light, is called the “additive primaries,” and these are 
-red, green, and blue. The three additive primaries with which we see—red, green, 
-and blue—are intimately connected to the three subtractive primaries—cyan, 
-magenta, and yellow—with which we print.
- 
- Therefore, we are writting a program that converts from addtive colors to 
-subtractive colors. The same as if when a printer driver converts them to the 
-appropriate values for the mix of inks in the printer. 
-  
- */
+
 package lightspectrum;
 
 import java.io.File;
